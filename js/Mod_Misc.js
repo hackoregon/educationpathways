1eb0223b0082fca09d1e79046b9093adf752f8c5
Aaron Campf <AaronCampf@Gmail.com>
2015-10-12 20:53:22 -0700

    Commir

--- js/Mod_Misc.js
+++ js/Mod_Misc.js
@@ -1,18 +1,20 @@
-﻿/// <reference path="jquery-1.11.3.min.js" />
+﻿///// <reference path="jquery-1.11.3.min.js" />
 
 
-function SetFilters(ID, ParameterName) {
-    var URL = "http://data.econw.com/sankey/meta/{0}/?format=json".format(ParameterName)
+//function SetFilters(ID, ParameterName) {
+//    var URL = "http://data.econw.com/sankey/meta/" + ParameterName + "/?format=json";
 
-    //var Data = $.getJSON(URL,)
+//    //var Data = $.getJSON(URL,)
 
-    var Data;
-    $.ajax({
-        dataType: "json",
-        url: URL,
-        data: Data
-    });
+//    var Data = {};
+//    $.ajax({
+//        dataType: "json",
+//        url: URL,
+//        data: Data
+//    });
 
+//    for (var Value in Data) {
+//        $("#" + ID).append("<option value=" + "'Value'" + ">" + Value +"</option>");
+//    }   
 
-
-}
+//}
